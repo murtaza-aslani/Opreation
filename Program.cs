d0c0b2ed250681303bb3bfe76f0e25f256756145
murtaza.aslani <murtaza.aslani1@gmail.com>
2023-05-26 09:45:37 +0430

    this code include math operation

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operation
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the seconde number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("---------------------");
+            // ضرب
+            int multiplicationResult = num1 * num2;
+            Console.WriteLine("multiplication: " + multiplicationResult);
+
+            // تفریق
+            int subtractionResult = num1 - num2;
+            Console.WriteLine("subtraction: " + subtractionResult);
+
+            // تقسیم
+            int divisionResult = num1 / num2;
+            Console.WriteLine("division: " + divisionResult);
+
+            Console.ReadLine();
+        }
+    }
+
+}
